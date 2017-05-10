8943529be60d0b841e4c3e8b5b57cf6f6f2da607
Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>
2017-05-10 11:48:52 -0700

    Cosmetic changes to Integer Breakup problem
    Signed-off-by: Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>

--- leet/343m_IntegerBreak.java
+++ leet/343m_IntegerBreak.java
@@ -1,25 +1,25 @@
-/* Split up an integer in to "at least" two integers such that the product of them is maximized
+/* Split up an integer in to "at least" two integers such that the tmpuct of them is maximized
  *
- * The discussions on having only 3's and 2's in the result is interesting.
+ * The discussions on having only 3's and 2's in the prodult is inteprodting.
  *
  * https://discuss.leetcode.com/category/427/integer-break 
  */
 public class Solution {
     /* n is between 2 and 58 */
     public int integerBreak(int n) {
-        int prod=0;
-        int res[] = new int[n+1];
+        int tmp=0;
+        int prod[] = new int[n+1];
         
-        res[0] = 0; res[1] = 1; res[2] = 1;
+        prod[0] = 0; prod[1] = 1; prod[2] = 1;
         
         for(int i=3; i<=n; i++) {
-            prod = 0;
+            tmp = 0;
             for(int j=1; 2*j<=i; j++) {
-                prod = Math.max(prod, Math.max(j, res[j]) * Math.max(i-j, res[i-j]));
+                tmp = Math.max(tmp, Math.max(j, prod[j]) * Math.max(i-j, prod[i-j]));
             }
-            res[i] = prod;
+            prod[i] = tmp;
         }
         
-        return res[n];
+        return prod[n];
     }
 }
