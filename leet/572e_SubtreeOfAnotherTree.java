28243ee433067f7ba130c9b56dfc6741802e17f2
Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>
2017-06-08 06:02:11 -0700

    Find if one tree is subtree of another tree
    Signed-off-by: Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>

---
+++ leet/572e_SubtreeOfAnotherTree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean match(TreeNode s, TreeNode t) {
+        if((s==null && t!=null) || (s!=null && t==null))
+            return false;
+        if(s==null && t==null)
+            return true;
+        
+        return s.val==t.val && match(s.left, t.left) && match(s.right, t.right);
+    }
+    
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if((s==null && t!=null) || (s!=null && t==null))
+            return false;
+        if(s==null && t==null)
+            return true;
+        
+        boolean result = match(s, t);
+        if(result)  return true;
+        
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+}
