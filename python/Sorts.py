38c7029003e64c8eda33e6cc93e22caac82163b3
Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>
2018-08-08 06:50:30 -0700

    Change string to all lowercase letters
    Signed-off-by: Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>

--- python/Sorts.py
+++ python/Sorts.py
@@ -1,3 +1,5 @@
+import random
+
 def QuickSort(arr, start, end):
     if start >= end:
         return
@@ -16,7 +18,7 @@ def Partition(arr, start, end):
     left = start + 1
     right = end
     
-    while left < right:
+    while left <= right:
         while left <= right and arr[left] < pivot_element:
             left = left + 1
         while left <= right and arr[right] >= pivot_element:
@@ -31,7 +33,18 @@ def Partition(arr, start, end):
 
     return right
 
+def random_numbers(N, limit):
+    arr = [random.randint(0, limit) for i in range(N)]
+    return arr
 
-arr = [15, 20, 5, 2, 31, 44, 64, 4, 7, 18, 55]
+
+
+#arr = [15, 20, 5, 2, 31, 44, 64, 4, 7, 18, 55]
+#QuickSort(arr, 0, len(arr)-1)
+#print arr
+
+arr = random_numbers(25, 50)
+print arr
+
 QuickSort(arr, 0, len(arr)-1)
 print arr
