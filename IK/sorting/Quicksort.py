75267f211c48531c37a6a66c5e019982cbb4d103
Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>
2018-11-19 15:13:49 -0800

    Quicksort implementation
    Signed-off-by: Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>

--- IK/sorting/Quicksort.py
+++ IK/sorting/Quicksort.py
@@ -1,56 +1,91 @@
-import random
+from random import randint
 
-def pivot(arr, start, end):
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
+def get_pivot_index(start, end):
     '''
-    returns index of a pivot element. Assumes valid start and end
+    start and end indices, are both inclusive
     '''
-    return start
+    return randint(start, end)
 
 
-def partition(arr, idx, start, end):
+def partition(arr, start, end, p_idx):
     '''
-    Given index of partition, rearrange elements and return final position of
-    pivot
+    Use Lumoto's algorithm. When paritioning is done, swap element at 'end'
+    where pivot is present with 'le'
+      +----------+-+----------+-+--------------+-+
+      |   <=     | |   >      | |    ??        | |
+      +----------+-+----------+-+--------------+-+
+                  ^            ^                      ^
+                  le           cur                   end
+    le = all elements less than or equal to pivot. It points to the next
+         position an element must go if it is <= pivot
+    cur = currently examining element
+    start = Well, start of the array
+    end = end of the array. This is the pivot element
+    p_idx = Initially, pivot is here. Swap it to the end, to make it easy to
+            partition.
     '''
-    l = start
-    r = end
+    swap(arr, end, p_idx)
+    le, cur, pivot = start, start, arr[end]
 
-    while True:
-        while l <= r and arr[l] <= arr[idx]: l += 1
-        while l <= r and arr[r] > arr[idx]: r -= 1
-
-        if l > r:
-            break
+    while cur < end:
+        if arr[cur] <= pivot:
+            swap(arr, le, cur)
+            le, cur = le+1, cur+1
         else:
-            arr[l], arr[r] = arr[r], arr[l]
+            cur += 1
 
-    arr[idx], arr[r] = arr[r], arr[idx]
-    return r
+    swap(arr, le, cur)
+    return le
 
 
-def k_small(arr, k, start, end):
+def test_partition(arr, p_idx):
+    '''
+    Given pivot index, confirm that all values before it are less than or equal
+    and all values later are greater than pivot
+    '''
+    for i in range(p_idx+1):
+        if(arr[i] > arr[p_idx]):
+            return False
+
+    for i in range(p_idx+1, len(arr)):
+        if(arr[i] <= arr[p_idx]):
+            return False
+
+    return True
+
+
+def Quicksort(arr, start, end):
     if start >= end:
-        return arr[start]
+        return
 
-    idx = pivot(arr, start, end)
-    pos = partition(arr, idx, start, end)
+    p_old = get_pivot_index(start, end)
+    p_final = partition(arr, start, end, p_old)
 
-    if pos == (k-1):
-        return arr[pos]
-
-    if pos < (k-1):
-        return k_small(arr, k, pos+1, end)
-    else:
-        return k_small(arr, k, start, pos-1)
+    Quicksort(arr, start, p_final-1)
+    Quicksort(arr, p_final+1, end)
 
 
-def k_smallest(arr, k):
-    return k_small(arr, k, 0, len(arr)-1)
+def test_quicksort(arr):
+    '''
+    Test for ascending array
+    '''
+    for i in range(1, len(arr)):
+        if arr[i-1] > arr[i]:
+            return False
 
+    return True
+        
 
 if __name__ == "__main__":
-    # arr = [3, 1, 0, 9, 7, 4]
-    for i in range(1, 1000):
-        arr = [random.randint(1, 100) for _ in range(25)]
-        # print sorted(arr)
-        print k_smallest(arr, 5)
+    for i in range(100):
+        arr_sz = 100
+        arr = [randint(-15, 15) for _ in range(arr_sz)]
+        Quicksort(arr, 0, arr_sz-1)
+        if not test_quicksort(arr):
+            print("Quicksort Failed")
+            print(arr)
+            break
