1966a80f12e8641c085324e8d1177f9d2ab56f40
Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>
2018-12-16 02:30:33 -0800

    LCA and Knights Tour problems
    Signed-off-by: Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>

--- IK/Graphs/HW/KnightsTour.py
+++ IK/Graphs/HW/KnightsTour.py
@@ -1,51 +1,33 @@
-#!/bin/python
+from collections import deque
 
 import sys
 
-WHITE = 0
-GRAY = 1
-BLACK = 2
-MIN_STEPS = 0
+def is_valid(rows, cols, x, y):
+    return 0 <= x and x < rows and 0 <= y and y < cols
 
-
-def within_bounds(rows, cols, x, y):
-    return x >= 0 and x < rows and y >= 0 and y < cols
-
-
-def moves(rows, cols, sr, sc, er, ec, path, visited):
-    global MIN_STEPS
-    if sr == er and sc == ec:
-        MIN_STEPS = min(MIN_STEPS, len(path))
-        print path
-        return
-
+def find_minimum_number_of_moves(rows, cols, sr, sc, er, ec):
     r = [-2, -2, -1, 1, -1, 1, 2, 2]
     c = [-1, 1, -2, -2, 2, 2, -1, 1]
 
-    path.append((sr, sc))
-    visited[sr][sc] = GRAY
+    q = deque([(sr, sc, 0)])
+    visited = set((sr, sc))
 
-    for i in range(0, 8):
-        x = sr + r[i]
-        y = sc + c[i]
+    while q:
+        row, col, count = q.popleft()
 
-        if not within_bounds(rows, cols, x, y):
-            continue
+        if (row, col) == (er, ec):
+            return count
 
-        if visited[x][y] == WHITE:
-            moves(rows, cols, x, y, er, ec, path, visited)
+        for i in range(8):
+            x, y = row + r[i], col + c[i]
+            if not is_valid(rows, cols, x, y):
+                continue
 
-    path.pop()
-    visited[sr][sc] = WHITE
+            if (x, y) not in visited:
+                visited.add((x, y))
+                q.append((x, y, count+1))
 
-
-def find_minimum_number_of_moves(rows, cols, sr, sc, er, ec):
-    global MIN_STEPS
-    visited = [[WHITE] * rows] * cols
-    MIN_STEPS = rows * cols
-    path = []
-    moves(rows, cols, sr, sc, er, ec, path, visited)
-    return MIN_STEPS
+    return -1
 
 
 if __name__ == "__main__":
@@ -61,10 +43,10 @@ if __name__ == "__main__":
     '''
     rows = 5
     cols = 5
-    start_row = 0
-    start_col = 0
+    start_row = 1
+    start_col = 1
     end_row = 4
-    end_col = 1
+    end_col = 4
 
     res = find_minimum_number_of_moves(rows, cols, start_row,
                                 start_col, end_row, end_col)
--- IK/Trees/HW/LCA.py
+++ IK/Trees/HW/LCA.py
@@ -7,14 +7,14 @@ This solution is from one of the Top submissions
 # this is needed for passing couple of test cases
 sys.setrecursionlimit(100000)
 
+def lcaNode(node, a, b):
+    if node in (None, a, b):
+        return node
+
+    lt = lcaNode(node.left, a, b)
+    rt = lcaNode(node.right, a, b)
+
+    return node if lt and rt else lt or rt
+
 def lca(root, a, b):
-    def lcaNode(node, a, b):
-        if node in (None, a, b):
-            return node
-
-        lt = lcaNode(node.left, a, b)
-        rt = lcaNode(node.right, a, b)
-
-        return node if lt and rt else lt or rt
-
     return lcaNode(root, a, b).data
